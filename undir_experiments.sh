3b364fd7c1f7a2b06565ffcd1f711fa864526244
周彦丞 <zhou-yc22@mails.tsinghua.edu.cn>
2025-12-23 13:20:13 +0000

    gogog o

--- undir_experiments.sh
+++ undir_experiments.sh
@@ -105,6 +105,36 @@ done
 
 
 cnt=0
+for dataset in ogbl_collab
+do
+    echo "Starting $dataset processing..."
+    mkdir -p ./logs/${dataset}
+    for ratio in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+    do
+        echo "Processing ratio $ratio for $dataset ..."
+        for turn in 1 2 3 4 5
+        do
+            # assign the devices
+            device=${devices[$((cnt % ${#devices[@]}))]}
+            cnt=$((cnt + 1))
+
+            # parallelly start the tasks
+            CUDA_VISIBLE_DEVICES=$device nohup python undir_main.py --dataset $dataset --split_ratio $ratio --turn $turn\
+            --h 14 --tp 16 --c 1 --neg 1 --gamma 3 --lam 1.5 --alpha 8 --epoch 50 --batch_size 8192 \
+            --seed $turn \
+            > ./logs/${dataset}/r${ratio}_s${turn}.log 2>&1 &
+
+            echo "Started turn $turn for ratio $ratio on device $device."
+        done
+        # waiting for this ratio to be finished
+        echo "Processing ratio $ratio for $dataset completed."
+        wait
+    done
+    echo "$dataset processing completed."
+    wait
+done
+
+cnt=0
 for dataset in cora citeseer pubmed internet kegg
 do
     echo "Starting $dataset processing..."
@@ -133,6 +163,83 @@ do
     echo "$dataset processing completed."
     wait
 done
+
+
+cnt=0
+for dataset in ogbl_collab
+do
+    echo "Starting $dataset processing..."
+    mkdir -p ./logs/${dataset}
+    for ratio in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+    do
+        echo "Processing ratio $ratio for $dataset ..."
+        for turn in 1 2 3 4 5
+        do
+            # assign the devices
+            device=${devices[$((cnt % ${#devices[@]}))]}
+            cnt=$((cnt + 1))
+
+            # parallelly start the tasks
+            CUDA_VISIBLE_DEVICES=$device nohup python undir_main.py --dataset $dataset --split_ratio $ratio --turn $turn\
+            --h 16 --tp 16 --c 1 --neg 1 --gamma 3 --lam 1 --alpha 57 --epoch 150 --batch_size 8192 \
+            --seed $turn \
+            > ./logs/${dataset}/r${ratio}_s${turn}.log 2>&1 &
+
+            echo "Started turn $turn for ratio $ratio on device $device."
+        done
+        # waiting for this ratio to be finished
+        echo "Processing ratio $ratio for $dataset completed."
+        wait
+    done
+    echo "$dataset processing completed."
+    wait
+done
+
+# >>> ogbl-collab <<< #
+dataset=ogbl-collab
+echo "Starting $dataset processing..."
+    ratio=0.02
+    echo "Processing ratio $ratio ..."
+    for turn in 0 1 2 3 4
+    do
+        # assign the devices
+        device=$turn
+
+        # parallelly start the tasks
+        CUDA_VISIBLE_DEVICES=$device nohup python undir_main.py --dataset $dataset --split_ratio $ratio --turn $turn\
+        --h 16 --tp 16 --c 3 --neg 1 --gamma 6 --lam 1.33 --alpha 11 --epoch 100 --batch_size 8192 \
+        --seed $turn \
+        > ./logs/${dataset}-r${ratio}-t${turn}.log 2>&1 &
+    done
+
+    # waiting for this ratio to be finished
+    wait
+    echo "Processing ratio $ratio completed."
+echo "$dataset processing completed."
+
+
+
+# >>> ogbl-ppa <<< #
+dataset=ogbl-ppa
+echo "Starting $dataset processing..."
+    ratio=0.02
+    echo "Processing ratio $ratio ..."
+    for turn in 0 1 2 3 4
+    do
+        # assign the devices
+        device=$turn
+
+        # parallelly start the tasks
+        CUDA_VISIBLE_DEVICES=$device nohup python undir_main.py --dataset $dataset --split_ratio $ratio --turn $turn\
+        --h 19 --tp 16 --c 1 --neg 1 --gamma 6 --lam 1 --alpha 24 --epoch 100 --batch_size 8192 \
+        --seed $turn \
+        > ./logs/${dataset}-r${ratio}-t${turn}.log 2>&1 &
+    done
+
+    # waiting for this ratio to be finished
+    wait
+    echo "Processing ratio $ratio completed."
+echo "$dataset processing completed."
 
 # evaluation
 python eval-speedup.py
---
+++ undir_scalability.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# making directories
+mkdir -p ./logs
+export PYTHONPATH=./
+
+
+# running experiments for synthetic datasets
+for power in 10 11 12 13 14 15 16 17 18 19
+do
+    num_node=$(echo "2^$power" | bc) # 2^power nodes
+
+    dataset='ER_'$num_node'_5'
+    logname='scaling_'$num_node
+
+    seed=1
+    device=1 # cuda:device
+
+    nohup python scaling/leaf_matching_scaling.py --dataset $dataset --device $device \
+        --h 16 --tp 16 --neg 1 --c 1 --alpha 16 --gamma 3 --epoch 600 --batch_size 32 --seed $seed \
+        > ./logs/$logname-$seed.log 2>&1
+    
+    wait
+done
+
+# running experiments for real-world datasets
+for ratio in 1.0 0.5 0.25 0.125 0.0625 0.03125 0.015625
+do
+    for seed in 1 2 3 4 5
+    do
+        device=0
+        CUDA_VISIBLE_DEVICES=$device nohup python lm.py \
+            --seed $turn --dataset ogbl_collab --epoch 160 \
+            --split_ratio $ratio --alpha 33 --gamma 5 --h 16 --tp 16 --c 3 --neg 1\
+            --batch_size 128 --pos_ratio 0.75\
+            > $home_path/collab_logs/$run_name.log 2>&1 &
+        cnt=$((cnt+1))
+        cnt=$((cnt%${#devices[@]}))
+
+        wait
+    done
+done
