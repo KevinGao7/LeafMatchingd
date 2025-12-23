e2573e1ed644f7b8bdac8c107e26abae614f5fa3
周彦丞 <zhou-yc22@mails.tsinghua.edu.cn>
2025-12-23 13:51:41 +0000

    It is time to transfer

--- undir_scalability.sh
+++ undir_scalability.sh
@@ -16,8 +16,8 @@ do
     seed=1
     device=1 # cuda:device
 
-    nohup python scaling/leaf_matching_scaling.py --dataset $dataset --device $device \
-        --h 16 --tp 16 --neg 1 --c 1 --alpha 16 --gamma 3 --epoch 600 --batch_size 32 --seed $seed \
+    nohup python undir_main.py --dataset $dataset --device $device \
+        --h 16 --tp 16 --neg 1 --c 1 --alpha 16 --gamma 3 --epoch 600 --batch_size 512 --seed $seed \
         > ./logs/$logname-$seed.log 2>&1
     
     wait
@@ -29,10 +29,10 @@ do
     for seed in 1 2 3 4 5
     do
         device=0
-        CUDA_VISIBLE_DEVICES=$device nohup python lm.py \
+        CUDA_VISIBLE_DEVICES=$device nohup python undir_main.py \
             --seed $turn --dataset ogbl_collab --epoch 160 \
             --split_ratio $ratio --alpha 33 --gamma 5 --h 16 --tp 16 --c 3 --neg 1\
-            --batch_size 128 --pos_ratio 0.75\
+            --batch_size 8192 --pos_ratio 0.75\
             > $home_path/collab_logs/$run_name.log 2>&1 &
         cnt=$((cnt+1))
         cnt=$((cnt%${#devices[@]}))
